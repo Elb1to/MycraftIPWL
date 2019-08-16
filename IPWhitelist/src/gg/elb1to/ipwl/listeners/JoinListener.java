d7666b9205dcf879010531a068e321ede5270746
Elb1to <50537641+Elb1to@users.noreply.github.com>
2019-08-16 11:39:06 -0500

    Removed innecesary code

--- IPWhitelist/src/gg/elb1to/ipwl/listeners/JoinListener.java
+++ IPWhitelist/src/gg/elb1to/ipwl/listeners/JoinListener.java
@@ -14,11 +14,11 @@ public class JoinListener implements Listener
         final String ip = e.getRealAddress().toString().replace("/", "");
         if (!Main.ip.contains(ip)) {
             e.setResult(PlayerLoginEvent.Result.KICK_OTHER);
-            e.setKickMessage(Main.instance.getConfig().getString("mensaje").replace("&", "ง"));
-            Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_RED + ChatColor.BOLD + "!" + ChatColor.DARK_GRAY + ")" + ChatColor.RED + "Intento de Bypass desde: " + ip + " \n                 IP del retrasado: " + e.getAddress().toString().replace("/", ""));
+            e.setKickMessage(Main.instance.getConfig().getString("mensaje").replace("&", "ยง"));
+            Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_RED + "!" + ChatColor.DARK_GRAY + ")" + ChatColor.RED + "Intento de Bypass desde: " + ip + " \n                     IP del retrasado: " + e.getAddress().toString().replace("/", ""));
             for (final Player p : Bukkit.getOnlinePlayers()) {
-                if (p.hasPermission(Main.instance.getConfig().getString("permiso").replace("&", "ง"))) {
-                    p.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_RED + ChatColor.BOLD + "!" + ChatColor.DARK_GRAY + ")" + ChatColor.RED + "Intento de Bypass desde: " + ip + " \nIP del retrasado: " + e.getAddress().toString().replace("/", ""));
+                if (p.hasPermission(Main.instance.getConfig().getString("permiso").replace("&", "ยง"))) {
+                    p.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_RED + "!" + ChatColor.DARK_GRAY + ")" + ChatColor.RED + "Intento de Bypass desde: " + ip + " \nIP del retrasado: " + e.getAddress().toString().replace("/", ""));
                 }
             }
         }

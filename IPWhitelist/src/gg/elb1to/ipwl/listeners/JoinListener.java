abce24eba948910573d543a8b0aba4dc2455814d
Elb1to <50537641+Elb1to@users.noreply.github.com>
2019-08-16 11:45:02 -0500

    Modified output messages and fixed small typo error

--- IPWhitelist/src/gg/elb1to/ipwl/listeners/JoinListener.java
+++ IPWhitelist/src/gg/elb1to/ipwl/listeners/JoinListener.java
@@ -15,10 +15,10 @@ public class JoinListener implements Listener
         if (!Main.ip.contains(ip)) {
             e.setResult(PlayerLoginEvent.Result.KICK_OTHER);
             e.setKickMessage(Main.instance.getConfig().getString("mensaje").replace("&", "§"));
-            Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_RED + "!" + ChatColor.DARK_GRAY + ")" + ChatColor.RED + "Intento de Bypass desde: " + ip + " \n                     IP del retrasado: " + e.getAddress().toString().replace("/", ""));
+            Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GRAY + "(" + ChatColor.DARK_RED + "!" + ChatColor.DARK_GRAY + ") " + ChatColor.RED + "Intento de Bypass desde: " + ip + " \n\t\t\tIP del retrasado: " + e.getAddress().toString().replace("/", ""));
             for (final Player p : Bukkit.getOnlinePlayers()) {
                 if (p.hasPermission(Main.instance.getConfig().getString("permiso").replace("&", "§"))) {
-                    p.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_RED + "!" + ChatColor.DARK_GRAY + ")" + ChatColor.RED + "Intento de Bypass desde: " + ip + " \nIP del retrasado: " + e.getAddress().toString().replace("/", ""));
+                    p.sendMessage(ChatColor.DARK_GRAY + "(" + ChatColor.DARK_RED + "!" + ChatColor.DARK_GRAY + ") " + ChatColor.RED + "Intento de Bypass desde: " + ip + " \nIP del retrasado: " + e.getAddress().toString().replace("/", ""));
                 }
             }
         }
